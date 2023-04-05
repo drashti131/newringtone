55ad25540150328eca0592cda01409d97ef374e7
Bhagvati italiya <bhagvatiitaliya001@gmail.com>
2023-04-05 12:44:10 +0530

    display front side

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ringtone;
 
 class UserController extends Controller
 {
     public function index(){
-        return view('home');
+        $data['ringtonedata']=ringtone::orderBy('r_id','DESC')->get();
+        return view('home',$data);
     }
 }
