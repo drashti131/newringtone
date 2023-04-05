8a141953f14e0bc1ec10ec5f43b8a61f4c87730f
Bhagvati italiya <bhagvatiitaliya001@gmail.com>
2023-04-05 09:57:52 +0530

    change in staging branch

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,6 @@ Route::get('/',[UserController::class,'index']);
 Auth::routes();
 
 Route::get('/admin', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Route::group(['middleware' => ['auth']], function() {
     Route::get('/admin/dashboard',[DashboardController::class,'dashboard']);
     Route::get('/admin/ringtone',[DashboardController::class,'ringtone']);
